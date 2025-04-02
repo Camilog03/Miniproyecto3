5673e620ce3adcb61a66da1de9d8512152db76b7
ManuUnivalle <manuela.delgado.correounivalle.edu.co>
2025-04-01 19:11:07 -0500

    Declaracion de metodos a usar en clase entrenador

--- Clase_entrenador/src/Entrenador.java
+++ Clase_entrenador/src/Entrenador.java
@@ -1,57 +1,47 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Entrenador {
-    private String coachName;
+    private String trainerName;
     private ArrayList<Pokemon> list = new ArrayList<Pokemon>();
     private static final byte MAX_POKEMON = 3;
 
-    public Entrenador(String coachName, ArrayList<Pokemon> list) {
-        this.coachName = coachName;
+    public Entrenador(String trainerName, ArrayList<Pokemon> list) {
+        this.trainerName = trainerName;
         this.list = new ArrayList<Pokemon>();
     }
     //Getters and setters
-    public String getCoachName() {
-        return coachName;
+    public String getTrainerName() {
+        return trainerName;
     }
-    public void setCoachName(String coachName) {
-        this.coachName = coachName;
+
+    public void setTrainerName(String trainerName) {
+        this.trainerName = trainerName;
     }
+
     public ArrayList<Pokemon> getList() {
         return list;
     }
+
     public void setList(ArrayList<Pokemon> list) {
         this.list = list;
     }
-    
-    //Method to add pokemon to team
-    public void addPokemon(Pokemon pokemon){
-        if(list.size() <MAX_POKEMON){
-            list.add(pokemon);
-            System.out.println(getCoachName() + " has choosen: "+ pokemon);
-        }
-        else{
-            System.out.println("Full team");
-        }        
+
+    public static byte getMaxPokemon() {
+        return MAX_POKEMON;
     }
-    //Method to choose a pokemon from all pokemons
-    public void choosePokemon(ArrayList<Pokemon> available){
-        Scanner sc = new Scanner(System.in);
-        byte option;
-        System.out.println("Available Pokémon:");
-        for (int i = 0; i < available.size(); i++) {
-            System.out.println((i + 1) + ". " + available.get(i));
-        }
     
-        while(list.size() < MAX_POKEMON){
-            System.out.println("Choose your pokemon number: ");
-            option = sc.nextByte();
-            if(option<1 || option > available.size() ){
-                System.out.println("Invalid choice");
-            }else{
-            addPokemon(available.get(option-1));
-            }
-        }
-        sc.close();
+    //Method to add pokemon to team randomly
+    public void addRandomPokemon(){
+             
     }
+    
+    //Method to add pokemon created by the trainer
+    public void addCreatedPokemon(){
+        
+    }
+    //Method to sort pokemon from least to most health
+    public void Sortlist(){
+        
+    }
+
 }
