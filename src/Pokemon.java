5ee93243c11a0aa81e87c87fe3abd1aaeb4d9d17
Srgabito123 <burbano.gabriel@correounivalle.edu.co>
2025-04-03 00:38:16 -0500

    Demo de la interfaz (interacciones del usuario), se crea la lógica básica a seguir en cuanto a nombrar entrenadores y crear equipos pokemón

---
+++ src/Pokemon.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Pokemon{
+    private String pokemonName;
+
+    public Pokemon(String pokemonName) {
+        this.pokemonName = pokemonName;
+    }
+
+    public String getPokemonName() {
+        return pokemonName;
+    }
+
+    public void setPokemonName(String pokemonName) {
+        this.pokemonName = pokemonName;
+    }
+
+    
+}
