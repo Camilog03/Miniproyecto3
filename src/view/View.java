5843726efdd02ad1862880f1d1715a08d4761583
ManuUnivalle <manuela.delgado@correounivalle.edu.co>
2025-05-17 22:52:59 -0500

    Creación de metodos de la interface

--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,19 @@
 package src.view;
 
 public interface View {
+    // Obtener información
+    void getNameTrainer();
+    void getAttack();
+    void choosePokemon();
+
+    // Mostrar información
+    void showMessage();
+    void showPokemons();
+    void showState();
+    void showAttack();
+    void showWinner();
+    
+    // Iniciar
+    void start();
+
 }
